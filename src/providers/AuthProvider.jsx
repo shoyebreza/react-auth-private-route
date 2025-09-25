f894927c2774b449d278f26f26d30cea864df7d1
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-09-25 20:43:39 +0600

    auth provider and auth context

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,76 @@
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import { auth } from '../firebase.init';
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext(null);
+
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const signInWithGoogle = () =>{
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const signOutUser = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('current User', currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        })
+
+        return () => {
+            unSubscribe();
+        }
+
+
+    }, [])
+
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signInUser,
+        signInWithGoogle,
+        signOutUser
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
+
+
+/**
+ * 1. create context with null as default
+ * 2. Create Provider
+ * 3. set a value with something (authInfo)
+ * 4. [ attention Please !!!!]
+ * 5. use the authProvider in the main.jsx
+ * 6. access the children inside the authProvider in the main.jsx
+ * 7. export AuthContext
+ * 
+*/
