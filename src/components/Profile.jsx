60099e5ec9f94c07576790515fa28132800e3877
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-09-26 12:01:09 +0600

    private route and auth

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">Update your Profile</h2>
+            <p>Donald tumi trump</p>
+        </div>
+    );
+};
+
+export default Profile;
